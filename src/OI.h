fec17cc4a9f0dd61ff218d9091ceec9c5d1a2076
ethanejohnsons <ethan@ejohnsons.com>
2017-05-16 10:54:39 -0500

    Added buttons to OI.h

--- src/OI.h
+++ src/OI.h
@@ -1,9 +1,23 @@
-#ifndef OI_H
-#define OI_H
+#ifndef OI_H_
+#define OI_H_
+
+#include <Buttons/JoystickButton.h>
+#include <Joystick.h>
 
 class OI {
 public:
 	OI();
+	frc::Joystick* GetJoystick();
+
+private:
+	frc::Joystick joy { 2 };
+
+	frc::JoystickButton d_up { &joy, 4 };
+	frc::JoystickButton d_right { &joy, 2 };
+	frc::JoystickButton d_down { &joy, 1 };
+	frc::JoystickButton d_left { &joy, 3 };
+	frc::JoystickButton left_b { &joy, 5 };
+	frc::JoystickButton right_b { &joy, 6 };
 };
 
 #endif  // OI_H
